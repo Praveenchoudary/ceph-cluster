8af695499e1391314097ab132b40aede94376475
Praveen Chinthala <96223263+Praveenchoudary@users.noreply.github.com>
2026-08-01 06:48:14 +0530

    Delete terraform-first-project directory

--- terraform-first-project/main.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_instance" "my_first_ec2" {
-  ami           = "ami-0f5ee92e2d63afc18"   # Amazon Linux 2023, ap-south-1 - verify it's current
-  instance_type = "t2.micro"
-
-  tags = {
-    Name = "my-first-terraform-ec2"
-  }
-}
