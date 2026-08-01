7b748edbc54e418b886544d528b71c66ec62e3d0
Praveen Chinthala <96223263+Praveenchoudary@users.noreply.github.com>
2026-08-01 06:47:02 +0530

    Delete terraform-state-basics directory

--- terraform-state-basics/main.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_instance" "web" {
-  ami           = "ami-0f5ee92e2d63afc18" # verify current AMI for your region
-  instance_type = "t2.micro"
-
-  tags = {
-    Name = "terraform-state-demo"
-  }
-}
