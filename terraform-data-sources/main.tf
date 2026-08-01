7204427b3cbdc933b4a8e940ac6d45d6f1d16e04
Praveen Chinthala <96223263+Praveenchoudary@users.noreply.github.com>
2026-08-01 06:46:25 +0530

    Delete terraform-data-sources directory

--- terraform-data-sources/main.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_instance" "web" {
-  ami           = data.aws_ami.amazon_linux.id
-  instance_type = "t2.micro"
-  subnet_id     = data.aws_subnets.default.ids[0]
-
-  tags = {
-    Name = "web-using-data-source"
-  }
-}
