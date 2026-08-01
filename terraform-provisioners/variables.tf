061013b8d67ea563f838988e4d72c151160da6a4
Praveen Chinthala <96223263+Praveenchoudary@users.noreply.github.com>
2026-08-01 06:47:23 +0530

    Delete terraform-provisioners directory

--- terraform-provisioners/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "key_name" {
-  description = "Existing EC2 key pair name for SSH access"
-  type        = string
-}
-
-variable "private_key_path" {
-  description = "Path to the local .pem private key file matching key_name"
-  type        = string
-}
