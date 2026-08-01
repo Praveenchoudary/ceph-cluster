ba832497c83f28d70c10f9080e6bc52ceba3e526
Praveen Chinthala <96223263+Praveenchoudary@users.noreply.github.com>
2026-08-01 06:47:13 +0530

    Delete terraform-remote-backend-full directory

--- terraform-remote-backend-full/app/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "mycompany-terraform-state-prod"   # must match bootstrap's state_bucket_name
-    key            = "prod/app/terraform.tfstate"
-    region         = "ap-south-1"
-    dynamodb_table = "terraform-locks"                  # must match bootstrap's lock_table_name
-    encrypt        = true
-  }
-}
